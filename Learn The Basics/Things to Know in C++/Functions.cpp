9f745b2702493bff26969a3c3e8b47c3541aeedb
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-14 01:17:50 +0530

    Add sayHello function to greet the user

---
+++ Learn The Basics/Things to Know in C++/Functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Function Definition
+void sayHello() {
+    cout << "Hello user!" << endl;
+}
+
+int main() {
+    sayHello(); // Calling the function
+    sayHello(); // Calling it again
+    return 0;
+}
