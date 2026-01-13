9d73b290efbf0924c6f7d94b1eaa9c2184b031d7
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-13 14:34:37 +0530

    Implement user input and output in C++

--- Learn The Basics/Things to Know in C++/User_Input_Output.cpp
+++ Learn The Basics/Things to Know in C++/User_Input_Output.cpp
@@ -1,1 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class Solution {
+public:
+    void printNumber() {
+        int n;
+        cin>>n;
+        cout<<n;
+    }
+};
+
+int main()
+{
+    Solution print;
+    print.printNumber();
+    return 0;
+}
