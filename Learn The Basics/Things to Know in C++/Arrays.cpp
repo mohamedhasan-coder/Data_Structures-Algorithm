b1d8b8672b3ab9456c2222fb1fa756f0140273da
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-14 01:14:12 +0530

    Add example of array usage in C++

--- Learn The Basics/Things to Know in C++/Arrays.cpp
+++ Learn The Basics/Things to Know in C++/Arrays.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    // Create an array of 5 integers
+    int marks[5] = {90, 85, 92, 78, 88};
+    
+    // Accessing the first student's marks
+    cout << marks[0] << endl; // Output: 90
+    
+    // Accessing the third student's marks
+    cout << marks[2] << endl; // Output: 92
+    
+    return 0;
+}
