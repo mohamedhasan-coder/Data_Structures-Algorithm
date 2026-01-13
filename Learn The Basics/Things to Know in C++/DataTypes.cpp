3d160b191a2d3359535abd3f9458f4fce1362836
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-13 14:49:06 +0530

    Add DataTypes.cpp to demonstrate variable usage
    This program demonstrates variable declaration and initialization in C++. It prints the values of different data types including int, float, char, bool, and string.

---
+++ Learn The Basics/Things to Know in C++/DataTypes.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Variable Declaration and intialazation
+    int Age = 20;
+    float Height = 5.5;
+    char Grade = 'A';
+    bool IsStudent = true;
+    string Name = "Hasan";
+
+    // Print 
+    cout<<"Name : "<<Name<<endl;
+    cout<<"Age : "<<Age<<endl;
+    cout<<"Height : "<<Height<<endl;
+    cout<<"Grade : "<<Grade<<endl;
+
+    return 0;
+}
