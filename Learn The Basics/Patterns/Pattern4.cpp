ed394ab6bdfb40afc91a5509ca7898920f976f97
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-15 14:14:42 +0530

    Implement pattern printing in Pattern4.cpp

--- Learn The Basics/Patterns/Pattern4.cpp
+++ Learn The Basics/Patterns/Pattern4.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class Solution{
+    public: 
+        void printPattern4(int n)
+        {
+            for (int i=1; i<=n; i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    cout<<i<<" ";
+                }
+                cout<<endl;
+            }
+        }
+};
+
+int main()
+{
+    int n;
+    cin>>n;
+    Solution Pattern;
+    Pattern.printPattern4(n);
+    return 0; 
+}
