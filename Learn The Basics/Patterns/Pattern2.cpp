4ebae4c6132f94462fce7bd3f6ee9affd9c1f41f
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-15 12:03:41 +0530

    Add Pattern2.cpp for printing a star pattern

---
+++ Learn The Basics/Patterns/Pattern2.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public: 
+        void printPattern2(int n)
+        {
+            for (int i=0; i<n; i++)
+            {
+                for(int j=0;j<=i;j++)
+                {
+                    cout<<"* ";
+                }
+                cout<<endl;
+            }
+        }
+};
+
+int main()
+{
+    int n;
+    cin>>n;
+    Solution Pattern;
+    Pattern.printPattern2(n);
+    return 0; 
+}
