643d7b0601d1c20bdb6c6595bef6a68c7ee095c1
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-15 22:15:51 +0530

    Add Pattern11.cpp to print alternating patterns

---
+++ Learn The Basics/Patterns/Pattern11.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public: 
+        void printPattern11(int n)
+        {
+            int start = 1;
+            for (int i=0;i<n;i++)
+            {
+                if(i%2 == 0)
+                {
+                    start = 1;
+                }
+                else 
+                {
+                    start = 0;
+                }
+                for (int j=0; j<=i; j++)
+                {
+                    cout<<start;
+                    start = 1 - start;
+                }
+            cout<<endl;
+            }
+        }
+};
+
+int main()
+{
+    int n;
+    cin>>n;
+    Solution Pattern;
+    Pattern.printPattern11(n);
+    return 0; 
+}
