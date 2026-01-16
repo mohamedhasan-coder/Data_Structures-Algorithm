c6d7b0bd63cd10729fc89c42ac8f0a60622394ab
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-16 19:57:02 +0530

    Create Pattern22.cpp

---
+++ Learn The Basics/Patterns/Pattern22.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void printPattern22(int n)
+    {
+        for (int i=0;i<2*n-1;i++)
+        {
+            for(int j=0;j<2*n-1;j++)
+            {
+                int top=i;
+                int left =j;
+                int right = (2*n -2) - j;
+                int down = (2*n - 2) - i;
+                cout<< (n-min(min(top,down), min(left, right)));
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main() {
+    int n;
+    cin>>n;
+    Solution Pattern;
+    Pattern.printPattern22(n);
+    return 0;
+}
