656a30f3033507137a2b6693c4e3f98949b06a0b
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-18 01:31:05 +0530

    Create UnOrderedSet.cpp

---
+++ Learn The Basics/C++_STL/UnOrderedSet.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unordered_set<int>st;
+    // lower_bound and upper_bound functions does not works
+    // rest all functions are same as above
+    // it does not stores in any particular oredr 
+    // it has a better complexity than set in most cases, except when collisan happens
+    return 0;
+}
