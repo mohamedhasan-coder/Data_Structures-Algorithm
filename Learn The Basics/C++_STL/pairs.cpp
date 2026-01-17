3e056ae7827a55421ce33b07018e4b050aed3550
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-01-17 18:01:18 +0530

    Add pairs.cpp to demonstrate pair usage in C++

---
+++ Learn The Basics/C++_STL/pairs.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    pair<int,int>p = {1,3};
+
+    cout<<p.first<<" "<<p.second<<endl;
+
+    pair <int, pair<int,int>>pp = {1,{3,4}};
+
+    cout<<pp.first<<" "<<pp.second.first<<" "<<pp.second.second<<endl;
+
+    pair<int,int>arr[] = {{1,2},{2,5}};
+
+    cout<<arr[1].second;
+    return 0;
+}
