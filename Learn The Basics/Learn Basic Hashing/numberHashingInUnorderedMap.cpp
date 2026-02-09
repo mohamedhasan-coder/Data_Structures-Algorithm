c8ae8dba849b535847e9cb666495ca456805d754
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-02-09 19:02:47 +0530

    Create numberHashingInUnorderedMap.cpp

---
+++ Learn The Basics/Learn Basic Hashing/numberHashingInUnorderedMap.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    unordered_map<int,int> mpp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];    
+        // Pre Compute
+        mpp[arr[i]]++;
+    }
+ 
+    // Iterate in the Map
+    for (auto it: mpp)
+    {
+        cout<<it.first<< "-->" << it.second <<endl;
+    }
+ 
+    int q;
+    cin>>q;
+    while (q--)
+    {
+        int number;
+        cin>>number;
+        // Fetch
+        cout<<mpp[number]<<endl;
+    }
+    
+    return 0;
+}
