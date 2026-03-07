8055e6e8b8ad09f9fa491d58d0ce61740ecc4421
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-03-07 13:47:39 +0530

    Update StringsAndArrays.cpp

--- StringsAndArrays.cpp
+++ StringsAndArrays.cpp
@@ -4,7 +4,7 @@ using namespace std;
 /* -------- ARRAY OPERATIONS -------- */
 class ArrayHelper { 
 public:  
-    void printArray(int arr[], int n) {
+    void printArray(int arr[], int n) { 
         for (int i = 0; i < n; i++) {
             cout << arr[i] << " ";
         }
